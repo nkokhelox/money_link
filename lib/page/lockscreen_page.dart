6c73417e53c5c78a3e4896e3878476580fca38fd
Nkokhelo Mhlongo <nkokhelox@gmail.com>
2022-07-13 10:28:36 +0200

    lockscreen

--- lib/page/lockscreen_page.dart
+++ lib/page/lockscreen_page.dart
@@ -10,22 +10,87 @@ class LockScreenPage extends StatefulWidget {
 }
 
 class _LockScreenPageState extends State<LockScreenPage> {
+  static String _hardCodedPattern = "123321";
+  final List<int> pressSequence = <int>[];
+
   @override
   Widget build(BuildContext context) {
-    return ElevatedButton.icon(
-      onPressed: unlockApp,
-      label: Text("UNLOCK APP"),
-      icon: Stack(
-        alignment: AlignmentDirectional.center,
-        children: [
-          Icon(Icons.shield, size: 100),
-          Icon(Icons.lock_outline, size: 60, color: Theme.of(context).primaryColor),
-        ],
+    return Scaffold(
+      body: InkWell(
+        onTap: singlePress,
+        onLongPress: longPress,
+        onDoubleTap: doublePress,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Row(
+              children: [],
+            ),
+            Stack(
+              alignment: AlignmentDirectional.center,
+              children: [
+                Icon(Icons.shield, size: 100),
+                Icon(Icons.lock_outline,
+                    size: 60, color: Theme.of(context).primaryColor),
+              ],
+            ),
+            Text(
+              "UNLOCK APP",
+              style: TextStyle(
+                color:
+                    pressSequence.length == 0 ? Colors.green : Colors.blueGrey,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
 
+  void singlePress() {
+    setState(() {
+      pressSequence.add(1);
+    });
+    verify();
+  }
+
+  void doublePress() {
+    setState(() {
+      pressSequence.add(2);
+    });
+    verify();
+  }
+
+  void longPress() {
+    setState(() {
+      pressSequence.add(3);
+    });
+    verify();
+  }
+
+  void verify() {
+    if (pressSequence.length == 6 &&
+        pressSequence.join() == _hardCodedPattern) {
+      pressSequence.clear();
+      unlockApp();
+    }
+
+    if (pressSequence.length < 6) {
+      return;
+    }
+
+    setState(() {
+      // shake the UI with a vibration.
+      pressSequence.clear();
+    });
+  }
+
   void unlockApp() {
-    Navigator.pushReplacement(context, PageTransition(curve: Curves.linear, type: PageTransitionType.bottomToTop, child: HomePage()));
+    Navigator.pushReplacement(
+        context,
+        PageTransition(
+            curve: Curves.linear,
+            type: PageTransitionType.bottomToTop,
+            child: HomePage()));
   }
 }
